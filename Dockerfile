6cb1bea1e0a22ee4c0c53c9b6494d3c32f1535e8
khvorosttt <khvorosttt@yandex.by>
2025-12-03 03:15:41 +0300

    feat: add Prisma ORM configuration

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM node:lts-alpine3.20
 EXPOSE 4000
 WORKDIR /usr/app
 COPY package*.json ./
-RUN npm install \
-    && npm cache clean --force \
-    && rm -rf /tmp/* /var/cache/apk/*
+RUN npm install && \
+    npm cache clean --force && \
+    rm -rf /tmp/* /var/cache/apk/* /root/.npm
+COPY prisma.config.ts ./
+COPY ./prisma /usr/app/prisma/
+RUN npx prisma generate
 COPY . .
 CMD ["npm", "start"]
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ import { SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const docsFile = await readFile(join(__dirname, '../doc/api.yaml'), 'utf-8');
+  const docsFile = await readFile(
+    join(process.cwd(), '/doc/api.yaml'),
+    'utf-8',
+  );
   SwaggerModule.setup('doc', app, parse(docsFile));
   console.log('Swagger start on path http://localhost:4000/doc');
   await app.listen(process.env.PORT || 4000);
