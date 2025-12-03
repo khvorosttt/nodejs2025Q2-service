e52be6c8c7aaa622c52ee39fc1df19b20c273a70
khvorosttt <khvorosttt@yandex.by>
2025-12-04 00:31:19 +0300

    feat: add database models and migrations with Prisma

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'dotenv';
+import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
